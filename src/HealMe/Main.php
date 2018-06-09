827901525e41300f90a1c1afe62c42644e1be146
SIVANNKH <39669152+SIVANNKH@users.noreply.github.com>
2018-06-08 21:21:19 -0700

    Update Main.php

--- src/HealMe/Main.php
+++ src/HealMe/Main.php
@@ -20,8 +20,11 @@ class Main extends PluginBase{
 
             case "healme":
                 if($sender instanceof Player){
-                    $sender->setHealth(20);
-		    $sender->sendMessage("Your heal has set!");
+			if($sender->hasPermission("healme.command"){
+				$sender->setHealth(20);
+				$sender->sendMessage("Your heal has set!");
+			}
+			   
                 }
             break;
 
